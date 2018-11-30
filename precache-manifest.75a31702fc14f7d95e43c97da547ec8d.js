ade32a18559e3432b255f2806533bcb72b5c2d56
string-Nicer <45462656+string-Nicer@users.noreply.github.com>
2018-11-30 17:25:30 +0800

    Updates

---
+++ precache-manifest.75a31702fc14f7d95e43c97da547ec8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5a6b71255dc832da585d5c94fa1a115",
+    "url": "/smart-brain/static/media/RobotoCondensed-Regular.a5a6b712.ttf"
+  },
+  {
+    "revision": "0eff21dfb82952397757",
+    "url": "/smart-brain/static/js/runtime~main.0eff21df.js"
+  },
+  {
+    "revision": "54823426ce0af335e701",
+    "url": "/smart-brain/static/js/main.54823426.chunk.js"
+  },
+  {
+    "revision": "a53fd9cb97d8f00e8115",
+    "url": "/smart-brain/static/js/1.a53fd9cb.chunk.js"
+  },
+  {
+    "revision": "54823426ce0af335e701",
+    "url": "/smart-brain/static/css/main.28cfc057.chunk.css"
+  },
+  {
+    "revision": "a53fd9cb97d8f00e8115",
+    "url": "/smart-brain/static/css/1.a9d83eae.chunk.css"
+  },
+  {
+    "revision": "51e010dfa43d52fe7b041a8ca58127b0",
+    "url": "/smart-brain/index.html"
+  }
+];
